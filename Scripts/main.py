535e9aef8b17d39ec20c683a98134290b8e0810c
rojasfuentes <rojasfuentesfc@gmail.com>
2023-05-05 17:06:16 -0500

    Changes added

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,21 +1,24 @@
 import Valija
 from datetime import date
+from tkinter import filedialog
 import pandas as pd
 import PySimpleGUI as sg
 
+
+
 Valija.main()
+file_path = filedialog.askopenfilename(title="Selecciona 'Calidad de la Orden'", filetypes=(("Archivo CSV", "*.csv"),))
 
 barcode_list = Valija.barcode_list
 no_valija = Valija.no_valija
 
 #example =['DESTRUMPNOV', '16004343', '16004344', 'DE-23-001', 'REEN-16004344']
 hoy = date.today()
-file_path = r'C:\Users\JFROJAS\Desktop\ValijaMDSTK\Archivos\Calidad de la orden (2).csv'
 
 df = pd.read_csv(file_path)
 df = df.iloc[:, [1, 2, 20, 24, 23, 33, 32]]
 
-formato_path = r'C:\Users\JFROJAS\Desktop\ValijaMDSTK\Archivos\SGC-000614 Relación de Acuses de Recibo Enviados.xlsx'
+formato_path = r'C:\Users\Mayo\Downloads\Valija\Archivos\SGC-000614 Relación de Acuses de Recibo Enviados.xlsx'
 formato = pd.read_excel(formato_path)
 book = pd.ExcelFile(formato_path)
 
@@ -34,7 +37,7 @@ if new_barcode_list:
     new_df['No. Valija'] = no_valija
     new_df['Fecha de registro de evidencia'] = hoy
 
-    new_df = new_df.iloc[:, [7, 0, 1, 2, 3, 4, 5, 6, 10, 8, 9]]
+    new_df = new_df.iloc[:, [7, 0, 1, 2, 3, 4, 6, 5, 10, 8, 9]]
     new_df.columns = ['No', 'Division', 'Delivery', 'Destino', 'Municipio', 'Estado', 'Fecha de embarque', 'Fecha de entrega', 'Fecha de registro de evidencia', 'No. Factura', 'No. Valija']
 
     with pd.ExcelWriter(formato_path, mode='a', if_sheet_exists='overlay') as writer:
