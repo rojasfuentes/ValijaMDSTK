86688a2a8875e72128302b3d5db6c9c7a13b9819
rojasfuentes <JFROJAS@MEDISTIK.CORP>
2023-05-05 15:32:18 -0600

    Primera Version

--- Scripts/Valija.py
+++ Scripts/Valija.py
@@ -1,47 +1,52 @@
 import PySimpleGUI as sg
 
+barcode_list = []
+no_valija = ''
 
 def main():
+    global no_valija
+    global barcode_list
+
     
     layout = [
+        [sg.Text('Número de Valija:'), sg.InputText(key='-VALIJA-')],
         [sg.Text('Escanea los códigos de barras:')],
         [sg.InputText(key='-INPUT-'), sg.Button('Agregar', key='-ADD-')],
         [sg.Listbox([], size=(30, 6), key='-LIST-', enable_events=True)],
-        [sg.Button('Editar', key='-EDIT-'), sg.Button('Eliminar', key='-DELETE-'), sg.Button('Salir')]
+        [sg.Button('Editar', key='-EDIT-'), sg.Button('Eliminar', key='-DELETE-'), sg.Button('Salir'),]
     ]
 
     window = sg.Window('Valija', layout)
-
-    text_list = []
 
     while True:
         event, values = window.read()
 
         if event == sg.WINDOW_CLOSED or event == 'Salir':
-            print(text_list)
+            no_valija = values['-VALIJA-']
+            print(no_valija)
             break
 
         if event == '-ADD-':
             text = values['-INPUT-']
             if text:
-                text_list.append(text[2:])
-                window['-LIST-'].update(text_list)
+                barcode_list.append(text[2:])
+                window['-LIST-'].update(barcode_list)
                 window['-INPUT-'].update('')
 
         elif event == '-EDIT-':
             selected = window['-LIST-'].get_indexes()
             if selected:
-                text = text_list[selected[0]]
+                text = barcode_list[selected[0]]
                 edited_text = sg.popup_get_text('Editar cadena de texto', text)
                 if edited_text:
-                    text_list[selected[0]] = edited_text
-                    window['-LIST-'].update(text_list)
+                    barcode_list[selected[0]] = edited_text
+                    window['-LIST-'].update(barcode_list)
 
         elif event == '-DELETE-':
             selected = window['-LIST-'].get_indexes()
             if selected:
-                del text_list[selected[0]]
-                window['-LIST-'].update(text_list)
+                del barcode_list[selected[0]]
+                window['-LIST-'].update(barcode_list)
 
     window.close()
 
--- Scripts/main.py
+++ Scripts/main.py
@@ -1,18 +1,46 @@
-example =['DESTRUMPNOV',
-'16004343',
-'16004344',
-'DE-23-001',
-'REEN-16004344']
-
+import Valija
+from datetime import date
 import pandas as pd
+import PySimpleGUI as sg
 
-file_path = r'C:\Users\Mayo\Downloads\Valija\Archivos\Calidad de la orden (2).csv'
+Valija.main()
+
+barcode_list = Valija.barcode_list
+no_valija = Valija.no_valija
+
+#example =['DESTRUMPNOV', '16004343', '16004344', 'DE-23-001', 'REEN-16004344']
+hoy = date.today()
+file_path = r'C:\Users\JFROJAS\Desktop\ValijaMDSTK\Archivos\Calidad de la orden (2).csv'
+
 df = pd.read_csv(file_path)
-df = df.iloc[:, [1, 2, 21, 24, 23, 33, 32]]
+df = df.iloc[:, [1, 2, 20, 24, 23, 33, 32]]
 
-print(df.head())
+formato_path = r'C:\Users\JFROJAS\Desktop\ValijaMDSTK\Archivos\SGC-000614 Relación de Acuses de Recibo Enviados.xlsx'
+formato = pd.read_excel(formato_path)
+book = pd.ExcelFile(formato_path)
 
-""" init = 0
-for i in df.keys():
-    print(str(init) +' ' + i)
-    init += 1 """
+limpiar = pd.DataFrame(index=range(100), columns=range(12))
+limpiar = limpiar.fillna('')
+
+not_found = [code for code in barcode_list if code not in df['ID_de_envío'].values]
+if not_found:
+    sg.popup(f"No se encontraron {len(not_found)} archivos: {', '.join(not_found)}")
+
+new_barcode_list = [code for code in barcode_list if code not in not_found]
+if new_barcode_list:
+    new_df = df.loc[df['ID_de_envío'].isin(new_barcode_list)]
+    new_df['No'] = range(1, len(new_df) + 1)
+    new_df['No. Factura'] = 'N/A'
+    new_df['No. Valija'] = no_valija
+    new_df['Fecha de registro de evidencia'] = hoy
+
+    new_df = new_df.iloc[:, [7, 0, 1, 2, 3, 4, 5, 6, 10, 8, 9]]
+    new_df.columns = ['No', 'Division', 'Delivery', 'Destino', 'Municipio', 'Estado', 'Fecha de embarque', 'Fecha de entrega', 'Fecha de registro de evidencia', 'No. Factura', 'No. Valija']
+
+    with pd.ExcelWriter(formato_path, mode='a', if_sheet_exists='overlay') as writer:
+        limpiar.to_excel(writer, sheet_name='VALIJA GENERAL', startrow=9, startcol=1, header=None, index=None)
+        new_df.to_excel(writer, sheet_name='VALIJA GENERAL', startrow=9, startcol=1, header=None, index=None)
+
+    print("¡Se ha completado la escritura del archivo!")
+else:
+    sg.popup("Todos los códigos de barras no se encontraron en el archivo, no se puede continuar con el proceso.")
